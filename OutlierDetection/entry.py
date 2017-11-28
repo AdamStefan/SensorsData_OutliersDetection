f21175b6ff39e0c16950499f3dbf4a7e3d33dd9a
AdamStefan <AdamStefan2003@gmail.com>
2017-11-28 22:47:00 +0200

    create a lstm for predicting the multivariate time series

--- OutlierDetection/entry.py
+++ OutlierDetection/entry.py
@@ -10,12 +10,20 @@ from statsmodels.tsa.arima_model import ARIMA
 from datetime import datetime
 import dataUtility 
 import dataAnalysis as da
+import neural_net_time_series as nn_ts
 
 dataPath = "..\\data2"
 
 sensorData = dataUtility.sensorData()
 sensorData.loadJsonData(dataPath)
 sensorData.dataFrame.to_csv('sensorData.csv', sep=',', encoding='utf-8') # save the data to a csv file
+frame_intervals = sensorData.computeFramesIntervals()
+#nn_ts.train_and_predict(frame_intervals[3],['Ext_Tem','Ext_Umi','Ext_Vvi','Int_Pu1','Int_Pu2','Int_Tem','Int_Umi'])
+
+##'Ext_Umi','Int_Pu1' ,'Int_Pu2'problema
+nn_ts.train_and_predict(frame_intervals[3],['Ext_Tem','Ext_Vvi','Int_Tem'])
+
+
 #frameIntervals = sensorData.computeFramesIntervals()
 analysis = da.dataAnalysis(sensorData)
 
